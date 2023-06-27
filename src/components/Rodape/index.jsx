ad8ed61d20ef5d4c403921fa72ec4a6ad823dba7
Davi <131279133+DaviRSS1@users.noreply.github.com>
2023-06-27 16:57:09 -0300

    Upando arquivos

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,13 @@
+import styles from './Rodape.module.css'
+
+function Rodape() {
+    return(
+        <footer className={styles.rodape}>
+            <h3>
+                Desenvolvido por Davi.
+            </h3>
+        </footer>
+    )
+}
+
+export default Rodape
